ffa1dbb4bdd5fe35a6a75277c2e5f6a1f946b694
eva13 <eva130504@gmail.com>
2025-05-15 22:20:33 +0300

    add logging and WeatherForecast test request

--- varnavina-ekaterina-kt-31-22/Program.cs
+++ varnavina-ekaterina-kt-31-22/Program.cs
@@ -1,21 +1,40 @@
+using Microsoft.AspNetCore.Mvc;
+using NLog;
+using NLog.Web;
+
 var builder = WebApplication.CreateBuilder(args);
+var logger = LogManager.Setup().LoadConfigurationFromAppSettings().GetCurrentClassLogger();
 
-// Add services to the container.
-builder.Services.AddRazorPages();
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
+try
 {
-    app.UseExceptionHandler("/Error");
+    builder.Logging.ClearProviders();
+    builder.Host.UseNLog();
+    builder.Services.AddControllers();
+    builder.Services.AddEndpointsApiExplorer();
+    builder.Services.AddSwaggerGen();
+
+    var app = builder.Build();
+
+    // Configure the HTTP request pipeline.
+    if (app.Environment.IsDevelopment())
+    {
+        app.UseSwagger();
+        app.UseSwaggerUI(options =>
+        {
+            options.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
+            options.RoutePrefix = string.Empty; // Swagger UI на корневом URL
+        });
+    }
+
+    app.UseAuthorization();
+    app.MapControllers();
+    app.Run();
 }
-app.UseStaticFiles();
-
-app.UseRouting();
-
-app.UseAuthorization();
-
-app.MapRazorPages();
-
-app.Run();
+catch (Exception ex)
+{
+    logger.Error(ex, "STOP");
+}
+finally
+{
+    LogManager.Shutdown();
+}
---
+++ varnavina-ekaterina-kt-31-22/WeatherForecast.cs
@@ -0,0 +1,10 @@
+﻿namespace varnavina_ekaterina_kt_31_22
+{
+    public class WeatherForecast
+    {
+        public DateOnly Date { get; set; }
+        public int TemperatureC { get; set; }
+        public string? Summary { get; set; }
+    }
+
+}
